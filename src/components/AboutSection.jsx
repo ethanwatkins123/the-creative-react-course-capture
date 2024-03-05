777cfc96f28772a3b05d171d7007efaf1ef311ac
ethanwatkins123 <ethanwatkins1000@gmail.com>
2024-03-05 13:34:25 -0300

    4. Services & Faq Section

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -3,7 +3,7 @@ import home1 from "../img/home1.png";
 
 export default function AboutSection() {
   return (
-    <div>
+    <>
       <div className="description">
         <div className="title">
           <div className="hide">
@@ -27,6 +27,6 @@ export default function AboutSection() {
       <div className="image">
         <img src={home1} alt="guy with a camera" />
       </div>
-    </div>
+    </>
   );
 }
--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 // page components
 import AboutSection from "../components/AboutSection";
+import ServicesSection from "../components/ServicesSection";
+import FaqSection from "../components/FaqSection";
 
 export default function AboutUs() {
   return (
-    <div>
+    <>
       <AboutSection />
-    </div>
+      <ServicesSection />
+      <FaqSection />
+    </>
   );
 }
